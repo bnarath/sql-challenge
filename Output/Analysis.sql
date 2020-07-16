8a84a72963ec10298aacea9f19c0d131d2cdb172
Bincy Narath <bincybaburajnarath@gmail.com>
2020-07-16 01:56:16 -0400

    Beautified tables for easy readability

--- Output/Analysis.sql
+++ Output/Analysis.sql
@@ -10,7 +10,8 @@ employee number, last name, first name, sex, and salary.
 
 DROP VIEW IF EXISTS employee_details_and_salary;
 CREATE VIEW employee_details_and_salary AS
-SELECT emp.emp_no, emp.last_name, emp.first_name, emp.sex, sal.salary
+SELECT emp.emp_no AS "employee number", 
+emp.last_name "last name", emp.first_name AS "first name", emp.sex, sal.salary
 FROM employees AS emp
 LEFT JOIN salaries AS sal
 ON emp.emp_no = sal.emp_no;
@@ -22,9 +23,9 @@ SELECT * FROM employee_details_and_salary LIMIT 10;
 for employees who were hired in 1986. */
 DROP VIEW IF EXISTS employees_hired_in_1986;
 CREATE VIEW employees_hired_in_1986 AS  
-SELECT emp.first_name, emp.last_name, emp.hire_date
-FROM employees as emp
-WHERE EXTRACT(YEAR FROM emp.hire_date) = 1986;
+SELECT first_name AS "first name", last_name AS "last name", hire_date AS "hire date"
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
 SELECT COUNT(*) FROM employees_hired_in_1986;
 SELECT * FROM employees_hired_in_1986 LIMIT 10;
@@ -34,7 +35,11 @@ information: department number, department name,
 the manager's employee number, last name, first name.*/
 DROP VIEW IF EXISTS dept_manager_details;
 CREATE VIEW dept_manager_details AS 
-SELECT dep_mngr.dept_no, depts.dept_name, dep_mngr.emp_no, emp.last_name, emp.first_name 
+SELECT dep_mngr.dept_no AS "department number", 
+depts.dept_name AS "department name",
+dep_mngr.emp_no AS "manager's employee number", 
+emp.last_name AS "manager's last name",
+emp.first_name AS "manager's first name"
 FROM dept_manager AS dep_mngr
 LEFT JOIN departments AS depts ON dep_mngr.dept_no = depts.dept_no
 LEFT JOIN employees AS emp ON dep_mngr.emp_no = emp.emp_no;
@@ -42,12 +47,16 @@ LEFT JOIN employees AS emp ON dep_mngr.emp_no = emp.emp_no;
 SELECT COUNT(*) FROM dept_manager_details;
 SELECT * FROM dept_manager_details LIMIT 10;
 
+
 /* List the department of each employee with the following information:
 employee number, last name, first name, and department name.
 */
 DROP VIEW IF EXISTS employee_dept_details;
 CREATE VIEW employee_dept_details AS
-SELECT emp.emp_no, emp.last_name, emp.first_name, dept.dept_name
+SELECT emp.emp_no AS "employee number", 
+emp.last_name AS "last name", 
+emp.first_name AS "first name",
+dept.dept_name AS "department name"
 FROM employees as emp
 LEFT JOIN dept_emp ON emp.emp_no = dept_emp.emp_no
 LEFT JOIN departments AS dept ON dept_emp.dept_no = dept.dept_no;
@@ -61,42 +70,48 @@ List first name, last name, and sex for employees
 whose first name is "Hercules" and last names begin with "B."
 */
 
-SELECT emp.first_name, emp.last_name, emp.sex
-FROM employees as emp
-WHERE LOWER(emp.first_name) = 'hercules'
-AND emp.last_name LIKE 'B%';
+SELECT first_name AS "first name", last_name AS "last name", sex
+FROM employees
+WHERE LOWER(first_name) = 'hercules'
+AND LOWER(last_name) LIKE 'b%';
 
 /*List all employees in the Sales department, 
 including their employee number, last name, first name, and department name.
 */
 
-SELECT emp.emp_no, emp.last_name, emp.first_name, dept.dept_name
+SELECT emp.emp_no AS "employee number", 
+emp.last_name AS "last name",
+emp.first_name AS "first name",
+dept.dept_name AS "department name"
 FROM employees as emp
 LEFT JOIN dept_emp ON emp.emp_no = dept_emp.emp_no
 LEFT JOIN departments AS dept ON dept_emp.dept_no = dept.dept_no
-WHERE dept.dept_name = 'Sales';
+WHERE dept.dept_name ILIKE 'sales';
 
 SELECT * FROM employee_dept_details
-WHERE dept_name = 'Sales';
+WHERE "department name" ILIKE 'sales';
 
 /*List all employees in the Sales and Development departments,
 including their employee number, last name, first name, and department name.
 */
 
-SELECT emp.emp_no, emp.last_name, emp.first_name, dept.dept_name
+SELECT emp.emp_no AS "employee number", 
+emp.last_name AS "last name", 
+emp.first_name AS "first name",
+dept.dept_name AS "department name"
 FROM employees as emp
 LEFT JOIN dept_emp ON emp.emp_no = dept_emp.emp_no
 LEFT JOIN departments AS dept ON dept_emp.dept_no = dept.dept_no
-WHERE dept.dept_name = 'Sales' OR dept.dept_name = 'Development';
+WHERE dept.dept_name ILIKE 'sales' OR dept.dept_name ILIKE 'development';
 
 SELECT * FROM employee_dept_details
-WHERE dept_name = 'Sales' OR dept_name = 'Development';
+WHERE "department name" ILIKE 'sales' OR "department name" ILIKE 'development';
 
 /*In descending order, list the frequency count of 
 employee last names, i.e., 
 how many employees share each last name.*/
 
-SELECT last_name, COUNT(*) AS freqency_count
+SELECT last_name AS "last name", COUNT(*) AS freqency_count
 FROM employees
 GROUP BY last_name
 ORDER BY freqency_count DESC;
